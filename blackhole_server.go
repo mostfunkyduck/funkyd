29814722b350b833b370365a97913cdddc7f414f
mostfunkyduck <mostfunkyduck@protonmail.com>
2020-05-10 07:47:16 -0400

    adding the code for the blackhole server and the server util lib

---
+++ blackhole_server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/miekg/dns"
+)
+
+type BlackholeServer struct {
+}
+
+func (s *BlackholeServer) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
+	// This is a copy of a test utility function in the dns library, designed as a basic echo function
+	m := new(dns.Msg)
+	m.SetReply(req)
+
+	m.Extra = make([]dns.RR, 1)
+	m.Extra[0] = &dns.TXT{Hdr: dns.RR_Header{Name: m.Question[0].Name, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0}, Txt: []string{"Hello world"}}
+	w.WriteMsg(m)
+}
---
+++ server.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"github.com/miekg/dns"
+	"strings"
+	"time"
+)
+
+func processResults(r dns.Msg, domain string, rrtype uint16) (Response, error) {
+	return Response{
+		Entry:        r,
+		CreationTime: time.Now(),
+		Key:          domain,
+		Qtype:        rrtype,
+	}, nil
+}
+
+func sendServfail(w ResponseWriter, duration time.Duration, r *dns.Msg) {
+	LocalServfailsCounter.Inc()
+	m := &dns.Msg{}
+	m.SetRcode(r, dns.RcodeServerFailure)
+	w.WriteMsg(m)
+	logQuery("servfail", duration, m)
+}
+
+func logQuery(source string, duration time.Duration, response *dns.Msg) error {
+	var queryContext LogContext
+	for i, _ := range response.Question {
+		for j, _ := range response.Answer {
+			answerBits := strings.Split(response.Answer[j].String(), " ")
+			queryContext = LogContext{
+				"name":         response.Question[i].Name,
+				"type":         dns.Type(response.Question[i].Qtype).String(),
+				"opcode":       dns.OpcodeToString[response.Opcode],
+				"answer":       answerBits[len(answerBits)-1],
+				"answerSource": fmt.Sprintf("[%s]", source),
+				"duration":     fmt.Sprintf("%s", duration),
+			}
+			QueryLogger.Log(NewLogMessage(
+				CRITICAL,
+				queryContext,
+				"",
+			))
+		}
+	}
+	return nil
+}
+
+func BuildClient() (*dns.Client, error) {
+	config := GetConfiguration()
+	cl := &dns.Client{
+		SingleInflight: true,
+		Timeout:        config.Timeout * time.Millisecond,
+		Net:            "tcp-tls",
+		TLSConfig: &tls.Config{
+			InsecureSkipVerify: config.SkipUpstreamVerification,
+		},
+	}
+	Logger.Log(NewLogMessage(
+		INFO,
+		LogContext{
+			"what": "instantiated new dns client in TLS mode",
+			"why":  "",
+			"next": "returning for use",
+		},
+		"",
+	))
+	return cl, nil
+}
